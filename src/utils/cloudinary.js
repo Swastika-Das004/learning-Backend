11e677a066da96bdc4834968501478b1e73959ef
Swastika-Das004 <dasswastika275@gmail.com>
2026-09-08 21:03:50 +0530

    try block of uploading file on cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -7,3 +7,19 @@ import fs from "fs"
   api_key: process.env.CLOUDINARY_API_KEY, 
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+      if (!localFilePath) return null
+
+      // upload the file on cloudinary
+      const response = await cloudinary.uploader.upload(localFilePath, {
+          resource_type: "auto"
+      })
+      // file has been uploaded successfully
+      console.log("file is uploaded on cloudinary", response.url);
+      return response;
+  } catch (error) {
+    
+  }
+}
