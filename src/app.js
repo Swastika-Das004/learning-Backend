1d30c06374c9c1190118b715dd67046fffd9a684
Swastika-Das004 <dasswastika275@gmail.com>
2026-09-19 00:58:47 +0530

    connected the user routes with app.js

--- src/app.js
+++ src/app.js
@@ -17,5 +17,18 @@ app.use(express.static("public"))
 
 app.use(cookieParser())
 
+
+//routes import
+
+import userRouter from './routes/user.routes.js'
+
+// routes declaration
+
+//app.use("/users", userRouter)
+app.use("/api/v1/users", userRouter)
+
+// so the url will look like->
+//  http://localhost:8000/api/v1/users/register
+
 export { app }
 
